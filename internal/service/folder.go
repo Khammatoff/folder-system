cbf409f02b4e89312ce68119f8adddb0efddd946
Danya <128917696+Khammatoff@users.noreply.github.com>
2025-09-10 19:49:39 +0500

    Update folder.go

--- internal/service/folder.go
+++ internal/service/folder.go
@@ -18,14 +18,6 @@ func NewFolderService(folderRepo repository.FolderRepository) FolderService {
 }
 
 func (s *folderService) GetRecommendedFolder(documentTypeID uint, sheetsCount int) (*entity.Folder, error) {
-	// This is a simplified recommendation.
-	// In a real application, you would join through FolderTypeAssignment to find a folder
-	// of a type that is allowed for this document type, and that has enough space.
-	// For this example, we'll assume the documentTypeID can be directly used to find a folder type.
-	// You would need to implement the logic to map document type to folder type.
-
-	// Placeholder: Assume documentTypeID corresponds to a folder type ID (1:1 for simplicity)
-	// In a real app, you'd have a separate method to get folder type from document type.
 	folderTypeID := documentTypeID
 
 	folder, err := s.folderRepo.FindFolderByTypeAndCapacity(folderTypeID, sheetsCount)
